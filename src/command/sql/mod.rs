fc1011b13b66979dead7fa2a8691843cdd5355d8
rgreen32 <ray.greenm@gmail.com>
2025-10-08 21:57:00 +0000

    seperated sql command execution into an excute file/fn and reletad command files

--- src/command/sql/mod.rs
+++ src/command/sql/mod.rs
@@ -1,3 +1,6 @@
+pub mod select;
+pub use select::select;
+
 use std::fs::File;
 use std::io::{SeekFrom, Seek, Read};
 use crate::models::schema::*;
@@ -14,15 +17,7 @@ pub fn execute(command_components: Vec<&str>, schema_data: &SchemaRAW, file: &mu
 
     let result = match target_table_schema_entry {
                         Some(entry) => {
-                            file.seek(SeekFrom::Start(schema_data.page_size as u64 * (entry.rootpage-1)  as u64)).expect("seek failed");
-                            let mut table_page_buf = vec![0; schema_data.page_size as usize];
-                            file.read_exact(&mut table_page_buf).expect("failed to read row size for table");
-                            let mut table_page: TablePage = match table_page_buf[0] {
-                                                                0x0D => TablePage::Leaf(LeafTablePage::from_bytes(&table_page_buf)),
-                                                                0x05 => TablePage::Interior(InteriorTablePage::from_bytes(&table_page_buf, file)),
-                                                                _    => panic!("unsupported page type"),
-                                                            };
-                            let table_rows = table_page.to_table_rows();
+                            let table_rows = select(command_components, &entry, &schema_data, file);
                             Ok(table_rows)
                         },
                         None => Err(SQLCommandError::UnknownTable(target_table.to_string()))
---
+++ src/command/sql/select.rs
@@ -0,0 +1,20 @@
+
+use std::fs::File;
+use std::io::{SeekFrom, Seek, Read};
+use crate::models::schema::*;
+use crate::models::tablepage::*;
+use crate::models::record::Record;
+
+
+pub fn select(command_components: Vec<&str>, entry: &SchemaRow, schema_data: &SchemaRAW, file: &mut File) -> Vec<Record> {
+    file.seek(SeekFrom::Start(schema_data.page_size as u64 * (entry.rootpage-1)  as u64)).expect("seek failed");
+    let mut table_page_buf = vec![0; schema_data.page_size as usize];
+    file.read_exact(&mut table_page_buf).expect("failed to read row size for table");
+    let mut table_page: TablePage = match table_page_buf[0] {
+                                        0x0D => TablePage::Leaf(LeafTablePage::from_bytes(&table_page_buf)),
+                                        0x05 => TablePage::Interior(InteriorTablePage::from_bytes(&table_page_buf, file)),
+                                        _    => panic!("unsupported page type"),
+                                    };
+    let table_rows = table_page.to_table_rows();
+    return table_rows;
+}
