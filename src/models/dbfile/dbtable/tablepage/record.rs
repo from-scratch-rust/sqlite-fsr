399c7d743f26573217709e32c6f00d157dce11b0
rgreen32 <ray.greenm@gmail.com>
2025-11-23 19:39:44 +0000

    implemented a new TableRow type to better represent table rows

--- src/models/dbfile/dbtable/tablepage/record.rs
+++ src/models/dbfile/dbtable/tablepage/record.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+
 
 #[derive(Debug)]
 pub struct Record {
@@ -6,50 +6,3 @@ pub struct Record {
     pub column_values: Vec<Vec<u8>>
 }
 
-impl fmt::Display for Record {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for i in 0..self.column_values.len() {
-            let value = if self.column_values[i].is_empty() { 
-                            "NULL".to_string()
-                        } else {
-                            self.column_values[i].iter().map(|&c| c as char).collect::<String>()
-                        };
-            write!(f, "{}", value)?;
-            if i != self.column_values.len() - 1 { write!(f, " ")?; }
-        }
-        Ok(())
-    } 
-}
-
-// Wrapper so a vector of `Record` can be printed to the console.
-// You cannot implement `Display` directly for `Vec<Record>` due to Rust's
-// orphan rules, so we provide a small newtype wrapper instead.
-pub struct Records(pub Vec<Record>);
-
-impl From<Vec<Record>> for Records {
-    fn from(v: Vec<Record>) -> Self {
-        Records(v)
-    }
-}
-
-impl fmt::Display for Records {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for (i, rec) in self.0.iter().enumerate() {
-            writeln!(f, "{}", rec)?;
-        }
-        Ok(())
-    }
-}
-
-impl std::ops::Deref for Records {
-    type Target = [Record];
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl std::ops::DerefMut for Records {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
